d578758aed3c30ca0b5ed44d2dd21cc8a0c74ad1
EdibleBible <ursvalboi@gmail.com>
2021-10-12 11:33:31 +0000

    nie dziala v0.1

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Tile extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+}
+
+class Grid extends React.Components {
+    constructor(props) {
+        super(props);
+    }
+}
+
+class Game extends React.Components {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+
+    }
+}
